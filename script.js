65ebdb8c95a9534de00533a73ddc1507369c2688
malik <mdmalik.b@gmail.com>
2019-07-07 14:05:27 +0800

    Firebase uploads

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 console.log('test')
 let image = document.getElementById('image')
+
+let uploader = document.getElementById('uploader')
+let fileButton = document.getElementById('fileButton')
 // Create a root reference
-let storageRef = firebase.storage().ref('tie.png');
-
-let imageBlob = new File(['tie'],'tie.png',{type: 'image/png'})
-
-storageRef.put(imageBlob).then(function (snapshot) {
-    console.log('Uploaded a blob or file!');
-});
 
 
-console.log(storageRef)
+let stringRef = firebase.storage().ref('string.txt')
+
+
+//listen to file upload
+
+fileButton.addEventListener("change", function(e){
+    let file = e.target.files[0];
+
+    let imageRef = firebase.storage().ref(file.name);
+    let task = imageRef.put(file);
+
+    task.on('state_changed',
+        function progress(snapshot){
+            let percentage = snapshot.bytesTransferred / snapshot.totalBytes * 100;
+            uploader.value = percentage
+
+        },
+
+        function err(err){
+            console.log(err)
+        },
+
+        function complete(){
+            console.log('File uploaded')
+        }
+    );
+})
+
+
+
